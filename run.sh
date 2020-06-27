2c14a030e61df83da9d925638952acdf22e3b261
jackattempts <38874200+jackattempts@users.noreply.github.com>
2020-06-27 20:07:39 +0530

    yala

---
+++ run.sh
@@ -0,0 +1,4 @@
+# python train.py --img 1024 --batch 2 --epochs 1 --data ../input/configyolo5/wheat0.yaml --cfg ../input/configyolo5/yolov5x.yaml --name yolov5x_fold0
+
+
+python train.py --img 224 --batch 2 --epochs 1 --data ../wheat_yolo_config/wheat0.yaml --cfg ../wheat_yolo_config/yolov5x.yaml --name yolov5x_fold0
